89981b50e078db4a52d801a25eae3b72eb02043c
Vedant2598 <vedant.vartak500@gmail.com>
2024-05-02 21:09:15 +0530

    Initial commit

--- src/Pages/Search/Search.js
+++ src/Pages/Search/Search.js
@@ -11,6 +11,7 @@ export default function Search() {
     const [arr, setarr] = useState([])
     const [search_value, setsearch_value] = useState('')
     const [searchResult, setsearchResult] = useState(false)
+    const [loading, setloading] = useState(false)
 
     //FUNCTIONS
     const searchProduct=async(e)=>{
@@ -21,15 +22,18 @@ export default function Search() {
         if(e){
             setsearch_value(e.target.value)
             searchs=e.target.value
+        }else{
+            setloading(true)
         }
         
        let result=await clientPostAxios(baseurl+"/users/search",{search:searchs})
-       
+       setsearchResult(false)
        if(result){
         //    arr=result.data
         setarr(result.data)
            //    console.log(arr)
            setsearchResult(true)
+           setloading(false)
         }
     }
 
@@ -47,15 +51,42 @@ export default function Search() {
 
            <div className='card_main_div'>
             <div className='card_main'>
+            {loading?
+                <div className='search-loading-parent'>
+                    <SearchLoadingBar size="1.5"/>  
+                </div> 
+            :
+
+            <>
                {searchResult&&
                <>
-                   {arr.map((a)=>(
-                    <><ProductCard product_name={a.name} product_price={a.price} product_description={a.description} product_discount={a.discount} rating={a.rating.$numberDecimal}/></>
-                   ))}
-               </>}
+                    {arr.length>0?
+                            <>
+                            {arr.map((a)=>(
+                                <><ProductCard product_name={a.name} product_price={a.price} product_description={a.description} product_discount={a.discount} rating={a.rating.$numberDecimal}/></>
+                            ))}
+                            </>
+                        :
+                        <div className='search-not-found'>
+                                No Search Found
+                            </div>
+                    }
+               </>
+            }
+            </>
+        }
             </div>
             </div>
         </div>
+        </>
+    )
+}
+
+
+const SearchLoadingBar=(props)=>{
+    return(
+        <>
+        <div className='search-loading' style={{width:`${props.size}cm`,height:`${props.size}cm`}}></div>
         </>
     )
 }
